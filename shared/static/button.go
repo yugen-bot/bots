c12ea1021c5cfebf2a6475776686e374d4b02419
Jurien Hamaker <whoami@jurien.dev>
2026-06-11 16:41:22 +0200

    chore: update support server link & kofi link

--- shared/static/button.go
+++ shared/static/button.go
@@ -5,10 +5,10 @@ import "github.com/disgoorg/disgo/discord"
 var (
 	ButtonDiscordSupportServer = discord.NewLinkButton(
 		"Join support server 👨‍⚕️",
-		"https://discord.gg/UttZbEd9zn",
+		"https://support.yugen.bot",
 	)
 	ButtonKofi = discord.NewLinkButton(
 		"Open Ko-Fi page ☕",
-		"https://ko-fi.com/jurienhamaker",
+		"https://kofi.yugen.bot",
 	)
 )
